f18ef536df8f4c2f22cc3ab1bd251f5bc35a5798
Mobina Kargar <119706098+Grmamobin@users.noreply.github.com>
2024-07-16 13:00:41 +0330

    testimonials part completed

--- src/components/testimonials/index.jsx
+++ src/components/testimonials/index.jsx
@@ -1,58 +1,108 @@
 import React from 'react';
 import Slider from "react-slick";
+import { useRef } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import './index.css';
 export default function Testimonials(){
+    const data = [
+        {
+            image:"src/assets/images/Bubble.svg",
+            name:"John Smith",
+            info:"Marketing Director at XYZ Corp"
+        },
+        {
+            image:"src/assets/images/Bubble.svg",
+            name:"John Smith",
+            info:"Marketing Director at XYZ Corp"
+        },
+        {
+            image:"src/assets/images/Bubble.svg",
+            name:"John Smith",
+            info:"Marketing Director at XYZ Corp"
+        },
+    ];
+    let sliderRef = useRef(null);
+    const next = () => {
+      sliderRef.slickNext();
+    };
+    const previous = () => {
+      sliderRef.slickPrev();
+    };
     const settings = {
-        className: "slider variable-width",
-        dots: true,
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+      };
+      const settings_2 = {
+        dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1
       };
+
+
     return(
-    <>
-    <div className='flex justify-center items-center w-[800px] pb-[85px] text-lg gap-[45px]'>
-        <img className='w-[241px]' src="src/assets/images/Label (1).svg" alt="" />
-        <p className='text-[18px]'>Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services</p>
-    </div> 
-    <div className='inline-flex pt-[84px] pb-[68px] items-center border rounded-[45px] bg-[var(--Dark,#191A23)]'>
-        <div className='flex items-start justify-between w-[1918px]'>
-        <Slider {...settings}>
-        <div className='flex flex-col flex-end gap-[20px]'>
-            <img className='w-[606px] h-[237.952px]' src="src/assets/images/Bubble.svg" alt="" />
-            <div>
-                <h4 className='font-spaceGrotesk font-medium text-[var(--Green,#B9FF66)]'>John Smith</h4>
-                <p className='text-white'>Marketing Director at XYZ Corp</p>
-            </div>
-            
+        <div>
+        <div className='flex justify-start items-center w-full pb-[85px] text-lg gap-[45px] flex-col lg:flex-row'>
+            <img className='w-[241px]' src="src/assets/images/Label (1).svg" alt="" />
+            <p className='text-[18px] text-center'>Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services</p>
         </div>
-        <div className='flex flex-col flex-end gap-[20px]'>
-            <img className='w-[606px] h-[237.952px]' src="src/assets/images/Bubble.svg" alt="" />
-            <div>
-                <h4 className='font-spaceGrotesk font-medium text-[var(--Green,#B9FF66)]'>John Smith</h4>
-                <p className='text-white'>Marketing Director at XYZ Corp</p>
+            <div className='pt-[84px] pb-[68px] border rounded-[45px] bg-[var(--Dark,#191A23)] lg:block hidden'>
+            <Slider
+            ref={slider => {
+            sliderRef = slider;
+            }}
+            {...settings}
+            >
+                {data.map((d)=>(
+            <div className='flex flex-col flex-end gap-[20px]' key={d.image}>
+                <img src={d.image} alt="" />
+                <div className='flex self-center flex-col pl-[100px]'>
+                    <h4 className='font-spaceGrotesk font-medium text-[var(--Green,#B9FF66)]'>{d.name}</h4>
+                    <p className='text-white'>{d.info}</p>
+                </div>
+                
             </div>
-            
-        </div>
-        <div className='flex flex-col flex-end gap-[20px]'>
-            <img className='w-[606px] h-[237.952px]' src="src/assets/images/Bubble.svg" alt="" />
-            <div>
-                <h4 className='font-spaceGrotesk font-medium text-[var(--Green,#B9FF66)]'>John Smith</h4>
-                <p className='text-white'>Marketing Director at XYZ Corp</p>
+                ))}
+                </Slider>
+                <div className='flex w-[564px] justify-between items-center pt-[125px] m-auto'>
+                    <img src="src/assets/images/Arrow left.svg" onClick={previous} alt="" />
+                    <img src="src/assets/images/Navigation stars.svg" alt="" />
+                    <img src="src/assets/images/Arrow right.svg" onClick={next} alt="" />
+                </div>  
             </div>
-            
-        </div>
 
 
-        
-        </Slider>
+            <div className='pt-[84px] pb-[68px] border rounded-[45px] bg-[var(--Dark,#191A23)] block lg:hidden'>
+            <Slider
+            ref={slider => {
+            sliderRef = slider;
+            }}
+            {...settings_2}
+            >
+                {data.map((d)=>(
+            <div className='flex flex-col flex-end justify-center items-center' key={d.image}>
+                <img className='w-full' src={d.image} alt="" />
+                <div className='flex self-center flex-col pl-[100px] pt-[20px]'>
+                    <h4 className='font-spaceGrotesk font-medium text-[var(--Green,#B9FF66)]'>{d.name}</h4>
+                    <p className='text-white'>{d.info}</p>
+                </div>
+                
+            </div>
+                ))}
+                </Slider>
+                <div className='flex justify-between items-center pt-[125px] px-[20px]'>
+                    <img src="src/assets/images/Arrow left.svg" onClick={previous} alt="" />
+                    <img src="src/assets/images/Navigation stars.svg" alt="" />
+                    <img src="src/assets/images/Arrow right.svg" onClick={next} alt="" />
+                </div> 
+            </div>
+
         </div>
-       
-    </div>
-    
-    </>
-    )
+)
 }
--- src/components/wholePage/index.jsx
+++ src/components/wholePage/index.jsx
@@ -4,7 +4,7 @@ import Services from '../services';
 import CaseStudies from '../caseStudies';
 import InProcess from '../inProcess';
 import Team from '../team';
-// import Testimonials from '../testimonials';
+import Testimonials from '../testimonials';
 import CotactUs from '../contactUs';
 import Footer from '../footer';
 export default function WholePage(){
@@ -16,7 +16,7 @@ export default function WholePage(){
         <section className="caseStudies px-[20px] pt-[100px] overflow-hidden"><CaseStudies/></section>
         <section className="inProcess pt-[100px] px-[20px]"><InProcess/></section>
         <section className="team pt-[100px] px-[20px]"><Team/></section>
-        {/* <section className="testimonials pt-[100px]"><Testimonials/></section>   STILL HAS BUG */}
+        <section className="testimonials pt-[100px] px-[20px]"><Testimonials/></section> 
         <section className="contactUs pt-[100px] px-[20px] "><CotactUs/></section>
         <section className="footer pt-[100px]"><Footer/></section>
     </div>
